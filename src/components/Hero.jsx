4a49934580dbe8200df708bcc2640c397f803225
kelly <africanbenchmarktech@gmail.com>
2023-05-12 01:14:06 -0400

    some work done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col md:flex-row w-4/5 justify-between
+   items-center mx-auti py-10 ">
+         <div className="md:w-3/6 w-full">
+               <div >
+                <h1 className='text-white text-5xl font-bold'>Buy and sell <br /> Digiatl Arts, <br />
+                <span className='text-gradient'>NFTs</span> Collections</h1>
+                <p>Mint and collect hottest Ntfs around</p>
+               </div>
+          </div>
+    </div>
+  )
+}
+
+export default Hero
